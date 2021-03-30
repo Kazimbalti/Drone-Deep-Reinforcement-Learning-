3c852b6cc86e16536c7dea9ac94abdd8c53c53e5
Muhammmad kazim <32608321+Kazimbalti@users.noreply.github.com>
2021-03-30 11:50:13 +0300

    Add files via upload

---
+++ agents/basic_agent.py
@@ -0,0 +1,45 @@
+import numpy as np
+from task import Task
+
+class Basic_Agent():
+    def __init__(self, task):
+        # Task (environment) information
+        self.task = task
+        self.state_size = task.state_size
+        self.action_size = task.action_size
+        self.action_low = task.action_low
+        self.action_high = task.action_high
+        self.action_range = self.action_high - self.action_low
+
+        # Score tracker
+        self.best_score = -np.inf
+
+        # Episode variables
+        self.reset_episode()
+
+    def reset_episode(self):
+        self.total_reward = 0.0
+        self.count = 0
+        state = self.task.reset()
+        return state
+
+    def step(self, reward, done):
+        # Save experience / reward
+        self.total_reward += reward
+        self.count += 1
+
+        # Learn, if at end of episode
+        if done:
+            self.learn()
+
+    def act(self, state):
+        new_thrust = 400
+        return [new_thrust for x in range(4)]
+
+    def learn(self):
+        # Learn by random policy search, using a reward-based score
+        self.score = self.total_reward / float(self.count) if self.count else 0.0
+        
+        if self.score > self.best_score:
+            self.best_score = self.score
+  
